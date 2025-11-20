fa2c0d40c65be600f7936850134694d392a41a8c
Erick-BC11 <a224216392@unison.mx>
2025-11-19 18:53:19 -0700

    Comenzamos proyecto de base de datos

---
+++ src/Conexion.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conexion {
+        // Credenciales según tu solicitud
+        private static final String URL = "jdbc:postgresql://localhost:5432/Insurance_Applications";
+        private static final String USER = "developer";
+        private static final String PASSWORD = "developer";
+
+        public static Connection getConnection() {
+            Connection conn = null;
+            try {
+                // Asegúrate de tener el driver de PostgreSQL en tu Build Path
+                conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Conexión exitosa a PostgreSQL.");
+            } catch (SQLException e) {
+                System.err.println("Error al conectar a la BD: " + e.getMessage());
+                e.printStackTrace();
+            }
+            return conn;
+        }
+    }
